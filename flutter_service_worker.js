c7187e090ba34aa4282e7fc23b13ac9609b94d17
cgutierr-zgz <carlosgutimo.3d@gmail.com>
2024-10-29 19:36:38 +0000

    deploy: a5eaf0ebbe5e5a33f7ad84fe6081b82a548b6589

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "c506024ba305f6b3c9a43d87f301bb5a",
-"flutter_bootstrap.js": "2b0bf18897b803881be75b7832d2ef24"};
+"flutter_bootstrap.js": "d018fa756ee50b6a4d513aeb124bdbe9"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
